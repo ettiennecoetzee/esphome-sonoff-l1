9962a9cbba81ef4b5f972db0165090d97f773bdf
Ettienne Coetzee <53652614+ettiennecoetzee@users.noreply.github.com>
2025-12-28 18:28:12 +0200

    Delete components directory

--- components/sonoff_l1/__init__.py
+++
@@ -1,5 +0,0 @@
-# Sonoff L1 external component for ESPHome
-# This component provides a custom light platform: `platform: sonoff_l1`
-
-IS_PLATFORM_COMPONENT = True
-CODEOWNERS = ["@ettiennecoetzee"]
